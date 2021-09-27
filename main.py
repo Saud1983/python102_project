05e494ffd31875ebe023ac80f2bea798e136c7d6
saud <s.s.alghamdi@hotmail.com>
2021-09-27 09:03:00 +0300

    Change the function by using Regex for entry and validation

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
-
-
 import datetime
 from datetime import date
+import re
 
 
-def new_entry():
-    """This function is for adding new entry to info dictionary"""
+def new_entry(entry):
 
-    add = True  # To give the user a chance to double check his entry before submitting it.
-    while add:
-        new_key = input("Please Enter the person name:\n")  # To save a new kay temporarily until the final approval.
-        new_value = input("Please Enter the birth day in this format DD-MM-YYYY:\n")  # To save a new value temporarily until the final approval.
-        # print(f"The person's name is: '{new_key}', and hers/his birth day on '{new_value}' \n")
-        # submit = input("Enter 'Y' to save the entry or simply enter any value to add them again\n").lower()
-        submit = "y"
-        if submit == "y":  # Save the new key/value pair to the a dictionary named all_persons.
-            new_key = new_key.lower().capitalize()  # It's the person's name that will be a key in the dictionary after giving it a proper formatting
+    pattern = re.compile(r"[a-zA-Z]+, ?(0?[1-9]|[12][0-9]|3[01])-(0?[1-9]|1[12]?)-\d{4}")
+    if re.match(pattern,entry):
+        entry_parts = entry.split(',')
+        new_key = entry_parts[0].lower().capitalize()  # It's the person's name that will be a key in the dictionary after giving it a proper formatting
+        new_value = entry_parts[1]
 
-            # Getting the current year out of the given birthday of the current person.
-            birthday_year = new_value.split('-')[2]  # The year of the current given birthday
+        # Getting the current year out of the given birthday of the current person.
+        birthday_year = new_value.split('-')[2]  # The year of the current given birthday
 
-            # Getting the name of the day out of the given birthday of the current person
-            birthday = new_value.split('-')  # Converting the current birthday to a list that has 3 elements
-            birthday = datetime.date(int(birthday[2]), int(birthday[1]), int(birthday[0]))  # Converting the current birthday from a string type to a datetime type in a proper order(Y,M,D)/type(integer) arguments that datetime.date work with.
-            day_name = birthday.strftime('%A')  # Retrieving the full day name as a string
-            all_persons[new_key] = [int(today) - int(birthday_year), day_name]  # Add a new key/value pair to the dictionary where the kay is the person's name, and the value is a list of 2 element, one is the person's age, and the second one is the day name
-            add = False
+        # Getting the name of the day out of the given birthday of the current person
+        birthday = new_value.split('-')  # Converting the current birthday to a list that has 3 elements
+        birthday = datetime.date(int(birthday[2]), int(birthday[1]), int(birthday[0]))  # Converting the current birthday from a string type to a datetime type in a proper order(Y,M,D)/type(integer) arguments that datetime.date work with.
+        day_name = birthday.strftime('%A')  # Retrieving the full day name as a string
+        all_persons[new_key] = [int(today) - int(birthday_year), day_name]  # Add a new key/value pair to the dictionary where the kay is the person's name, and the value is a list of 2 element, one is the person's age, and the second one is the day name
+
+    else:
+        print(f'Invalid Entry {entry}')
 
 
 def unpacking(name, info):
@@ -41,7 +37,7 @@ current_person = {}  # Initial value of a global dictionary that save only one k
 today = date.today().strftime('%Y')  # Getting the year of the current date from the Operating System
 
 while running:
-    new_name = input("Enter '0' for printing the result, or enter '1' to add a new name :\n")
+    new_name = input("Please enter the name and value according to this format 'name, DD-MM-YYYY, or '0' for printing the result:\n")
 
     # choices
     if new_name == "0":
@@ -51,9 +47,13 @@ while running:
             current_person['info'] = value  # It's a list contains 2 elements [ age, the full name of the day ]
             unpacking(**current_person)
 
-        print(f"The oldest one is {max(all_persons, key=all_persons.get)}")
-        print(f"The youngest one is {min(all_persons, key=all_persons.get)}")
+        if len(all_persons) > 1:
+            print(f"The oldest one is {max(all_persons, key=all_persons.get)}")
+            print(f"The youngest one is {min(all_persons, key=all_persons.get)}")
+
+        else:
+            print('There is no oldest or youngest person')
         print(f"Total People: {len(all_persons)}")
         running = False
     else:
-        new_entry()
+        new_entry(new_name)
